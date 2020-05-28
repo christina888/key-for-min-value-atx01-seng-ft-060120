18a6e6671abbd77d8010510b78bc71ea83c25b73
Christina Chianelli <cechianelli@gmail.com>
2020-05-28 03:41:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,6 @@ def key_for_min_value(name_hash)
     if value < lowest_value || lowest_value == nil
       lowest_value = value 
       lowest_key = key 
-    else name_hash = {}
-      return nil
     end
   end 
 lowest_key  
